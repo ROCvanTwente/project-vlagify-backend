d7594d7d0de528c5acf0d2d1e0e601bffda84fa6
Salim <0357057@student.rocvantwente.nl>
2026-02-10 12:04:10 +0100

    Nieuwe Sln/Project toegevoegd

---
+++ Vlagify/API/Controllers/Test.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Test : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult GetTests()
+        {
+            return Ok(new
+            {
+                value = "Test"
+            });
+        }
+    }
+}
