697ccadfe309d0872ee6adee23365663f6d09900
Sem Christenhusz <1357767@student.rocvantwente.nl>
2026-03-17 12:02:39 +0100

    Added AspCoreIdentity

---
+++ Vlagify/API/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using API.Models;
+
+namespace API.Data
+{
+	public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+	{
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+			: base(options)
+		{
+		}
+
+		protected override void OnModelCreating(ModelBuilder builder)
+		{
+			base.OnModelCreating(builder);
+			// Hier kun je eventueel eigen configuraties toevoegen
+		}
+	}
+}
--- Vlagify/API/Program.cs
+++ Vlagify/API/Program.cs
@@ -1,22 +1,39 @@
+using API.Data; // Zorg dat dit naar jouw DbContext map verwijst
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// --- 1. Database configuratie ---
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+	options.UseSqlServer(connectionString));
+
+// --- 2. Identity configuratie ---
+// Hiermee koppel je de Identity tabellen aan je DbContext
+builder.Services.AddIdentityApiEndpoints<IdentityUser>()
+	.AddEntityFrameworkStores<ApplicationDbContext>();
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// --- 3. Middleware volgorde ---
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+	app.MapOpenApi();
 }
 
 app.UseHttpsRedirection();
 
+// Belangrijk: UseAuthentication moet VÓÓR UseAuthorization staan!
+app.UseAuthentication();
 app.UseAuthorization();
+
+// --- 4. Identity Endpoints mappen ---
+// Dit zorgt voor de /register en /login routes in je API
+app.MapIdentityApi<IdentityUser>();
 
 app.MapControllers();
 
